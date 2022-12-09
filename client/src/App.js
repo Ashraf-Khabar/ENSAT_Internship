a679e38abbbf7fe94b4258c15ab9c3895a6f9606
Achraf Khabar <95541956+Ashraf-Khabar@users.noreply.github.com>
2022-12-09 17:03:54 +0100

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,37 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 import Login from "./components/Login";
 import Navbar from "./components/Navbar";
 import Register from "./components/Register";
 import MainNavBar from "./components/MainNavBar";
+import './App.css';
+import Page_404 from "./components/Page_404";
 
 function App() {
   return (
-      <BrowserRouter>
-        <MainNavBar/>
-        <Switch>
-          <Route exact path="/login">
-            <Login/>
-          </Route>
-          <Route path="/register">
-            <Register/>
-          </Route>
-          <Route path="/dashboard">
-            <Navbar/>
-            <Dashboard/>
-          </Route>
-        </Switch>
-      </BrowserRouter>
+      <Router>
+          <div className="App">
+              <Switch>
+
+                  <Route exact path="/login">
+                      <MainNavBar/>
+                      <Login/>
+                  </Route>
+                  <Route path="/register">
+                      <MainNavBar/>
+                      <Register/>
+                  </Route>
+                  <Route path="/dashboard">
+                      <Navbar/>
+                      <Dashboard/>
+                  </Route>
+                  <Route path="*">
+                      <MainNavBar/>
+                      <Page_404/>
+                  </Route>
+              </Switch>
+          </div>
+      </Router>
   );
 }
 
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, {useState} from 'react'
 import axios from 'axios';
-import { useHistory } from 'react-router-dom';
+import {Link, useHistory} from 'react-router-dom';
 
 const Navbar = () => {
     const history = useHistory();
-
+    const [navbar, setNavbar] = useState(false);
     const Logout = async () => {
         try {
             await axios.delete('http://localhost:5000/logout');
@@ -15,35 +15,74 @@ const Navbar = () => {
     }
 
     return (
-        <nav className="navbar is-light" role="navigation" aria-label="main navigation">
-            <div className="container">
-                <div className="navbar-brand">
-                    <a className="navbar-item" href="https://bulma.io">
-                        <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" alt="logo" />
-                    </a>
-
-                    <a href="/" role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-                        <span aria-hidden="true"></span>
-                        <span aria-hidden="true"></span>
-                        <span aria-hidden="true"></span>
-                    </a>
-                </div>
-
-                <div id="navbarBasicExample" className="navbar-menu">
-                    <div className="navbar-start">
-                        <a href="/" className="navbar-item">
-                            Home
-                        </a>
-                    </div>
-
-                    <div className="navbar-end">
-                        <div className="navbar-item">
-                            <div className="buttons">
-                                <button onClick={Logout} className="button is-light">
-                                    Log Out
-                                </button>
-                            </div>
+        <nav className="w-full bg-white shadow">
+            <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
+                <div>
+                    <div className="flex items-center justify-between py-3 md:py-5 md:block">
+                        <Link to="/">
+                            <h2 className="text-2xl font-bold">LOGO dialna</h2>
+                        </Link>
+                        <div className="md:hidden">
+                            <button
+                                className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
+                                onClick={() => setNavbar(!navbar)}
+                            >
+                                {navbar ? (
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="w-6 h-6"
+                                        viewBox="0 0 20 20"
+                                        fill="currentColor"
+                                    >
+                                        <path
+                                            fillRule="evenodd"
+                                            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                            clipRule="evenodd"
+                                        />
+                                    </svg>
+                                ) : (
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="w-6 h-6"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                        strokeWidth={2}
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            d="M4 6h16M4 12h16M4 18h16"
+                                        />
+                                    </svg>
+                                )}
+                            </button>
                         </div>
+                    </div>
+                </div>
+                <div>
+                    <div
+                        className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+                            navbar ? "block" : "hidden"
+                        }`}
+                    >
+                        <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+                            <li className="text-gray-600 hover:text-blue-600">
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li className="text-gray-600 hover:text-blue-600">
+                                <Link to="/stages">Stages</Link>
+                            </li>
+                            <li className="text-gray-600 hover:text-blue-600">
+                                <Link to="/about-us">About US</Link>
+                            </li>
+                            <li className="text-gray-600 hover:text-blue-600">
+                                <Link to="contact-us">Contact US</Link>
+                            </li>
+                            <li className="text-gray-600 hover:text-blue-600">
+                                <button onClick={Logout} >Log out</button>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>
