8ce42874d87e9dd08e0b6f83e1c4e00b9c90b264
Achraf Khabar <95541956+Ashraf-Khabar@users.noreply.github.com>
2022-12-09 18:54:01 +0100

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -6,30 +6,36 @@ import Register from "./components/Register";
 import MainNavBar from "./components/MainNavBar";
 import './App.css';
 import Page_404 from "./components/Page_404";
+import Home from "./components/Home";
 
 function App() {
   return (
       <Router>
           <div className="App">
-              <Switch>
-
-                  <Route exact path="/login">
-                      <MainNavBar/>
-                      <Login/>
-                  </Route>
-                  <Route path="/register">
-                      <MainNavBar/>
-                      <Register/>
-                  </Route>
-                  <Route path="/dashboard">
-                      <Navbar/>
-                      <Dashboard/>
-                  </Route>
-                  <Route path="*">
-                      <MainNavBar/>
-                      <Page_404/>
-                  </Route>
-              </Switch>
+              <div className="relative h-screen overflow-hidden bg-indigo-900 opacity-93">
+                  <Switch>
+                      <Route exact path="/">
+                          <MainNavBar/>
+                          <Home/>
+                      </Route>
+                      <Route path="/login">
+                          <MainNavBar/>
+                          <Login/>
+                      </Route>
+                      <Route path="/register">
+                          <MainNavBar/>
+                          <Register/>
+                      </Route>
+                      <Route path="/dashboard">
+                          <Navbar/>
+                          <Dashboard/>
+                      </Route>
+                      <Route path="*">
+                          <MainNavBar/>
+                          <Page_404/>
+                      </Route>
+                  </Switch>
+              </div>
           </div>
       </Router>
   );
