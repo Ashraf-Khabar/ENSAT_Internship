1886e4a1b0fc11f3842f80f36a6979d9c0630c54
Achraf Khabar <95541956+Ashraf-Khabar@users.noreply.github.com>
2022-12-11 18:40:23 +0100

    Create Stages.js

---
+++ client/src/components/dashboardComponents/Stages.js
@@ -0,0 +1,17 @@
+
+
+const Stages = () => {
+
+
+    return (
+        <div>
+            <center>
+                <h2>
+                    All stages
+                </h2>
+            </center>
+        </div>
+    );
+}
+
+export default Stages ;
