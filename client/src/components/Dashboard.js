84c2e998f97520fb8d82c45f2c29d21325ed1e00
Achraf Khabar <95541956+Ashraf-Khabar@users.noreply.github.com>
2022-12-11 22:59:01 +0100

    commit

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -57,103 +57,87 @@ const Dashboard = () => {
     }
 
     return (
-        <div className="min-h-screen bg-gray-100">
-            <div
-                className="sidebar min-h-screen w-[3.35rem] overflow-hidden border-r hover:w-56 hover:bg-white hover:shadow-lg">
-                <div className="flex h-screen flex-col justify-between pt-2 pb-6">
-                    <div>
-                        
-                        <ul className="mt-6 space-y-2 tracking-wide">
-                            <li className="min-w-max">
-                                <a href="#" aria-label="dashboard"
-                                   className="relative flex items-center space-x-4 bg-gradient-to-r from-sky-600 to-cyan-400 px-4 py-3 text-white">
-                                    <svg className="-ml-1 h-6 w-6" viewBox="0 0 24 24" fill="none">
-                                        <path
-                                            d="M6 8a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2V8ZM6 15a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2v-1Z"
-                                            className="fill-current text-cyan-400 dark:fill-slate-600"></path>
-                                        <path
-                                            d="M13 8a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2V8Z"
-                                            className="fill-current text-cyan-200 group-hover:text-cyan-300"></path>
-                                        <path
-                                            d="M13 15a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2v-1Z"
-                                            className="fill-current group-hover:text-sky-300"></path>
-                                    </svg>
-                                    <span className="-mr-1 font-medium">Dashboard</span>
-                                </a>
-                            </li>
-                            <li className="min-w-max">
-                                <a href="#"
-                                   className="bg group flex items-center space-x-4 rounded-full px-4 py-3 text-gray-600">
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20"
-                                         fill="currentColor">
-                                        <path className="fill-current text-gray-300 group-hover:text-cyan-300"
-                                              fill-rule="evenodd"
-                                              d="M2 6a2 2 0 012-2h4l2 2h4a2 2 0 012 2v1H8a3 3 0 00-3 3v1.5a1.5 1.5 0 01-3 0V6z"
-                                              clip-rule="evenodd"/>
-                                        <path className="fill-current text-gray-600 group-hover:text-cyan-600"
-                                              d="M6 12a2 2 0 012-2h8a2 2 0 012 2v2a2 2 0 01-2 2H2h2a2 2 0 002-2v-2z"/>
-                                    </svg>
-                                    <span className="group-hover:text-gray-700">Categories</span>
-                                </a>
-                            </li>
-                            <li className="min-w-max">
-                                <a href="#"
-                                   className="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600">
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20"
-                                         fill="currentColor">
-                                        <path className="fill-current text-gray-600 group-hover:text-cyan-600"
-                                              fill-rule="evenodd"
-                                              d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z"
-                                              clip-rule="evenodd"/>
-                                        <path className="fill-current text-gray-300 group-hover:text-cyan-300"
-                                              d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z"/>
-                                    </svg>
-                                    <span className="group-hover:text-gray-700">Reports</span>
-                                </a>
-                            </li>
-                            <li className="min-w-max">
-                                <a href="#"
-                                   className="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600">
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20"
-                                         fill="currentColor">
-                                        <path className="fill-current text-gray-600 group-hover:text-cyan-600"
-                                              d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"/>
-                                        <path className="fill-current text-gray-300 group-hover:text-cyan-300"
-                                              d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"/>
-                                    </svg>
-                                    <span className="group-hover:text-gray-700">Other data</span>
-                                </a>
-                            </li>
-                            <li className="min-w-max">
-                                <a href="#"
-                                   className="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600">
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20"
-                                         fill="currentColor">
-                                        <path className="fill-current text-gray-300 group-hover:text-cyan-300"
-                                              d="M4 4a2 2 0 00-2 2v1h16V6a2 2 0 00-2-2H4z"/>
-                                        <path className="fill-current text-gray-600 group-hover:text-cyan-600"
-                                              fill-rule="evenodd"
-                                              d="M18 9H2v5a2 2 0 002 2h12a2 2 0 002-2V9zM4 13a1 1 0 011-1h1a1 1 0 110 2H5a1 1 0 01-1-1zm5-1a1 1 0 100 2h1a1 1 0 100-2H9z"
-                                              clip-rule="evenodd"/>
-                                    </svg>
-                                    <span className="group-hover:text-gray-700">Finance</span>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="w-max -mb-3">
-                        <a href="#" className="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 group-hover:fill-cyan-600"
-                                 viewBox="0 0 20 20" fill="currentColor">
-                                <path fill-rule="evenodd"
-                                      d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z"
-                                      clip-rule="evenodd"/>
+        <div className="flex">
+            <div className="flex w-2/5 md:w-1/4 h-screen bg-white">
+                <div className="mx-auto py-10">
+                    <ul>
+                        <li className="flex space-x-2 mt-10 cursor-pointer hover:text-[#EC5252] duration-150">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                                 stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                      d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"/>
                             </svg>
-                            <span className="group-hover:text-gray-700">Settings</span>
-                        </a>
-                    </div>
+                            <span className="font-semibold">Home</span>
+                        </li>
+                        <li className="flex space-x-2 mt-10 cursor-pointer hover:text-[#EC5252] duration-150">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                                 stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                      d="M16 4v12l-4-2-4 2V4M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"/>
+                            </svg>
+                            <span className="font-semibold">All Courses</span>
+                        </li>
+                        <li className="flex space-x-2 mt-10 cursor-pointer hover:text-[#EC5252] duration-150">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                                 stroke="currentColor">
+                                <path d="M12 14l9-5-9-5-9 5 9 5z"/>
+                                <path
+                                    d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"/>
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                      d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"/>
+                            </svg>
+                            <span className="font-semibold">My Course</span>
+                        </li>
+                        <li className="flex space-x-2 mt-10 cursor-pointer hover:text-[#EC5252] duration-150">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                                 stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                      d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"/>
+                            </svg>
+                            <span className="font-semibold">Profile</span>
+                        </li>
+                        <li className="flex space-x-2 mt-10 cursor-pointer hover:text-[#EC5252] duration-150">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                                 stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                      d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"/>
+                            </svg>
+                            <span className="font-semibold">Setthing</span>
+                        </li>
+                        <button className="w-full mt-10 bg-[#EC5252] rounded-full py-1.5 text-white">Learn</button>
+                    </ul>
                 </div>
             </div>
+            <main className=" min-h-screen w-full">
+                <nav className="flex justify-between px-10 bg-white py-6">
+                    <div className="flex items-center bg-gray-100 px-4 py-2 rounded-md space-x-3 w-96">
+                        <input type="text" placeholder="search" className="bg-gray-100 outline-none w-full"/>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 cursor-pointer text-gray-500"
+                             fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"/>
+                        </svg>
+                    </div>
+                    <div className="flex items-center">
+                        <img style={{margin: '10px'}} className="w-8 rounded-full"
+                             src="https://imagez.tmz.com/image/f7/1by1/2021/12/14/f7703994b69d48ca802df55729a2325c_xl.jpg"
+                             alt="Elon Musk"/>
+                        <p>{name}</p>
+                    </div>
+                </nav>
+                <div className="w-full ">
+                    <h1 className="text-2xl font-semibold"></h1>
+                    <div className="md:flex mt-4 space-x-4  justify-center">
+                        <div className="h-96 bg-gradient-to-r from-indigo-600 to-purple-500 flex items-end rounded-md">
+                            <p className="text-lg text-indigo-50">How to do Basic Jumping and how to landing safely</p>
+                        </div>
+                        <div className="h-96 bg-gradient-to-r from-yellow-600 to-red-500 flex items-end rounded-md">
+                            <p className="text-lg text-indigo-50">How to do Basic Jumping and how to landing safely</p>
+                        </div>
+
+                    </div>
+                </div>
+            </main>
         </div>
     );
 }
