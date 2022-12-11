bb9c0598bc0566044c58d7fc0a29b8089f538285
Achraf Khabar <95541956+Ashraf-Khabar@users.noreply.github.com>
2022-12-11 02:48:06 +0100

    COMMIT

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react'
 import axios from 'axios';
 import {Link, useHistory} from 'react-router-dom';
 import jwt_decode from "jwt-decode";
+import appLogo from "../img/appLogo.png";
 
 const Navbar = ({userId, setUserId}) => {
     const history = useHistory();
@@ -45,7 +46,7 @@ const Navbar = ({userId, setUserId}) => {
                 <div>
                     <div className="flex items-center justify-between py-3 md:py-5 md:block">
                         <Link to="/">
-                            <h2 className="text-2xl font-bold">LOGO dialna</h2>
+                            <img src={appLogo} style={{ width: '100px', height: '100px' }}  alt="logo"/>
                         </Link>
                         <div className="md:hidden">
                             <button
