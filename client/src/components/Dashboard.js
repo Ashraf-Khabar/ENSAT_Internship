be9010bb5512dfab9d00083bf206e0f64857b138
Achraf Khabar <95541956+Ashraf-Khabar@users.noreply.github.com>
2022-12-12 16:39:35 +0100

    Update Dashboard.js

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -35,20 +35,20 @@ const Dashboard = () => {
 
     const axiosJWT = axios.create();
 
-    axiosJWT.interceptors.request.use(async (config) => {
-        const currentDate = new Date();
-        if (expire * 1000 < currentDate.getTime()) {
-            const response = await axios.get('http://localhost:5000/token');
-            config.headers.Authorization = `Bearer ${response.data.accessToken}`;
-            setToken(response.data.accessToken);
-            const decoded = jwt_decode(response.data.accessToken);
-            setName(decoded.name);
-            setExpire(decoded.exp);
-        }
-        return config;
-    }, (error) => {
-        return Promise.reject(error);
-    });
+    // axiosJWT.interceptors.request.use(async (config) => {
+    //     const currentDate = new Date();
+    //     if (expire * 1000 < currentDate.getTime()) {
+    //         const response = await axios.get('http://localhost:5000/token');
+    //         config.headers.Authorization = `Bearer ${response.data.accessToken}`;
+    //         setToken(response.data.accessToken);
+    //         const decoded = jwt_decode(response.data.accessToken);
+    //         setName(decoded.name);
+    //         setExpire(decoded.exp);
+    //     }
+    //     return config;
+    // }, (error) => {
+    //     return Promise.reject(error);
+    // });
 
     const getUsers = async () => {
         const response = await axiosJWT.get('http://localhost:5000/users', {
