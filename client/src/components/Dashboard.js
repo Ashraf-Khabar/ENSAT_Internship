afdd6a3c2ff025d56fedf922540a6f314501b00d
Achraf Khabar <95541956+Ashraf-Khabar@users.noreply.github.com>
2022-12-12 15:00:43 +0100

    commit

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -91,14 +91,14 @@ const Dashboard = () => {
                             </svg>
                             <span className="font-semibold">My Course</span>
                         </li>
-                        <li className="flex space-x-2 mt-10 cursor-pointer hover:text-[#EC5252] duration-150">
+                        <Link to="/dashboard/profile" className="flex space-x-2 mt-10 cursor-pointer hover:text-[#EC5252] duration-150">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24"
                                  stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                       d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"/>
                             </svg>
-                            <span className="font-semibold">Profile</span>
-                        </li>
+                            <Link className="font-semibold" to="/dashboard/profile">Profile</Link>
+                        </Link>
                         <li className="flex space-x-2 mt-10 cursor-pointer hover:text-[#EC5252] duration-150">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24"
                                  stroke="currentColor">
--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,10 +1,125 @@
+import {Link, useHistory} from "react-router-dom";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+import jwt_decode from "jwt-decode";
 
 const Profile = () => {
+    const [name, setName] = useState('');
+    const [token, setToken] = useState('');
+    const [expire, setExpire] = useState('');
+    const [users, setUsers] = useState([]);
+    const history = useHistory();
+    const [active, setActive] = useState(false);
+
+    useEffect(() => {
+        refreshToken();
+        getUsers();
+    }, []);
+
+    const refreshToken = async () => {
+        try {
+            const response = await axios.get('http://localhost:5000/token');
+            setToken(response.data.accessToken);
+            const decoded = jwt_decode(response.data.accessToken);
+            setName(decoded.name);
+            setExpire(decoded.exp);
+        } catch (error) {
+            if (error.response) {
+                history.push("/");
+            }
+        }
+    }
+
+    const axiosJWT = axios.create();
+
+    axiosJWT.interceptors.request.use(async (config) => {
+        const currentDate = new Date();
+        if (expire * 1000 < currentDate.getTime()) {
+            const response = await axios.get('http://localhost:5000/token');
+            config.headers.Authorization = `Bearer ${response.data.accessToken}`;
+            setToken(response.data.accessToken);
+            const decoded = jwt_decode(response.data.accessToken);
+            setName(decoded.name);
+            setExpire(decoded.exp);
+        }
+        return config;
+    }, (error) => {
+        return Promise.reject(error);
+    });
+
+    const getUsers = async () => {
+        const response = await axiosJWT.get('http://localhost:5000/users', {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+        setUsers(response.data);
+    }
+
     return (
-        <div>
-            <h1>
-                25
-            </h1>
+        <div className="flex">
+            <div className="flex w-2/5 md:w-1/4 h-screen bg-white">
+                <div className="mx-auto py-10">
+                    <ul>
+                        <li className="flex space-x-2 mt-10 cursor-pointer hover:text-[#EC5252] focus:text-[#EC5252] duration-150">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                                 stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                      d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"/>
+                            </svg>
+                            <Link>Home</Link>
+                        </li>
+                        <li className="flex space-x-2 mt-10 cursor-pointer hover:text-[#EC5252] active:text-[#EC5252] duration-150">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                                 stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                      d="M16 4v12l-4-2-4 2V4M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"/>
+                            </svg>
+                            <span className="font-semibold">All Courses</span>
+                        </li>
+                        <li className="flex space-x-2 mt-10 cursor-pointer hover:text-[#EC5252] duration-150">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                                 stroke="currentColor">
+                                <path d="M12 14l9-5-9-5-9 5 9 5z"/>
+                                <path
+                                    d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"/>
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                      d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"/>
+                            </svg>
+                            <span className="font-semibold">My Course</span>
+                        </li>
+                        <Link to="/dashboard/profile" className="flex space-x-2 mt-10 cursor-pointer hover:text-[#EC5252] duration-150">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                                 stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                      d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"/>
+                            </svg>
+                            <Link className="font-semibold" to="/dashboard/profile">Profile</Link>
+                        </Link>
+                        <li className="flex space-x-2 mt-10 cursor-pointer hover:text-[#EC5252] duration-150">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                                 stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                      d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"/>
+                            </svg>
+                            <span className="font-semibold">Setthing</span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div className=" min-h-screen w-full">
+
+                <div className="w-full ">
+                    <nav className="flex justify-between px-1 bg-white py-1">
+                        <div className="flex items-center">
+                            <img style={{margin: '10px', width: '45px', height: 'px'}} className="w-8 rounded-full"
+                                 src="https://imagez.tmz.com/image/f7/1by1/2021/12/14/f7703994b69d48ca802df55729a2325c_xl.jpg"
+                                 alt="Elon Musk"/>
+                            <p>{name}</p>
+                        </div>
+                    </nav>
+                </div>
+            </div>
         </div>
     );
 }
