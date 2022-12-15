0c7872656710203170ae35da548764801a16ca17
Achraf Khabar <95541956+Ashraf-Khabar@users.noreply.github.com>
2022-12-15 01:05:51 +0100

    commit

--- server/controllers/Users.js
+++ server/controllers/Users.js
@@ -15,6 +15,12 @@ export const getUsers = async(req, res) => {
 
 export const Register = async(req, res) => {
     const { name, email, password, confPassword, role} = req.body;
+
+    const emailToken = jwt.sign({
+        email: email,
+        exp: Math.floor(Date.now() / 1000) + (60 * 60) // 1 hour expiration time
+    }, 'your-secret-key');
+
     if(password !== confPassword) return res.status(400).json({msg: "Password and Confirm Password do not match"});
     const salt = await bcrypt.genSalt();
     const hashPassword = await bcrypt.hash(password, salt);
@@ -25,7 +31,28 @@ export const Register = async(req, res) => {
             password: hashPassword,
             role : role
         });
-        res.json({msg: "Registration Successful"});
+
+        // Send a confirmation email to the user
+        const mailOptions = {
+            from: 'ashrafkhabaradm@gmail.com', // sender address
+            to: email, // list of receivers
+            subject: 'Registration Confirmation', // Subject line
+            text: `Hi ${name},
+        Thank you for registering with us. Your email address is ${email}.
+        Click the following link to confirm your email address:
+        http://your-site.com/confirm-email?token=${emailToken}
+      `
+        };
+
+        // Send the email
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.log(error);
+                res.status(500).json({msg: "Failed to send confirmation email"});
+            } else {
+                res.json({msg: "Registration and confirmation email sent successfully"});
+            }
+        });
     } catch (error) {
         console.log(error);
     }
