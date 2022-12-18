aeb5cef99f06fb19fd8476317b55f14376a6102d
Kaouthar-bouslim <Bouslim.kaouthar@gmail.com>
2022-12-18 23:48:23 +0100

    Update Addoffer.js

--- client/src/components/dashboardComponents/employer/Addoffer.js
+++ client/src/components/dashboardComponents/employer/Addoffer.js
@@ -103,70 +103,110 @@ const Addoffer = () => {
     
             </ul>
         </div>
-    </div>
-    <div className=" min-h-screen w-full bg-white">
-    <div className="flex flex-row w-full">
-                <div className="flex flex-1 flex-col items-center justify-center px-10 relative">
-                    <div className="flex flex-1 flex-col  justify-center space-y-5 max-w-md">
-                        <div className="flex flex-col space-y-2 text-center">
-                            <h2 className="text-3xl md:text-4xl font-bold">Add an offer </h2>
-                            <p className="text-md md:text-xl">test </p>
-                        </div>
-                        <form onSubmit = {Addoffer} >
-                            <div className="flex flex-col max-w-md space-y-5">
-                                <input onChange={(e) => setTitre(e.target.value)} type="text" placeholder="Titre"
-                                       className="flex px-3 py-2 md:px-4 md:py-3 border-2 border-black rounded-lg font-medium placeholder:font-normal"/>
-                                       <select onChange={(e) => setSector(e.target.value)}
-                                        className="flex px-3 py-2 md:px-4 md:py-3 border-2 border-black rounded-lg font-medium placeholder:font-normal">
-                                    <option value="IT">IT</option>
-                                    <option value="RH">RH</option>
-                                    <option value="MARKETING">MARKETING</option>
-                                </select>
-                                <select onChange={(e) => setType(e.target.value)}
-                                        className="flex px-3 py-2 md:px-4 md:py-3 border-2 border-black rounded-lg font-medium placeholder:font-normal">
-                                    <option value="PFE">PFE</option>
-                                    <option value="PFA">PFA</option>
-                                    <option value="OBSERVATION">OBSERVATION</option>
-                                 </select>
-                                 <div className="flex w-max gap-4" onChange={(e) => setPaid(e.target.value)}>
-                                    <p>Paid</p>
-                                    <input type="radio" id="Yes" value="true"></input>
-                                     
-                                     <label for="Yes">Yes</label>
-                                     <input type="radio" id="No" value="false"></input>
-                                    
-                                        <label for="No">No</label>
-                                   </div>
-                                 <textarea onChange={(e) => setDescription(e.target.value)} placeholder="Description"
-                                       className="flex px-3 py-2 md:px-4 md:py-3 border-2 border-black rounded-lg font-medium placeholder:font-normal"/>
-                                <input onChange={(e) => setNbrcand(e.target.value)} type="number"
-                                       placeholder="number"
-                                       className="flex px-3 py-2 md:px-4 md:py-3 border-2 border-black rounded-lg font-medium placeholder:font-normal"/>
-                                <input onChange={(e) => setDatedebut(e.target.value)} type="date"
-                                     
-                                      
-                                       min="2018-01-01" max="2023-12-31"
-                                       className="flex px-3 py-2 md:px-4 md:py-3 border-2 border-black rounded-lg font-medium placeholder:font-normal"/>
-                                <input onChange={(e) => setDatefin(e.target.value)} type="date"
-                                        
-                                       
-                                       min="2018-01-01" max="2023-12-31"
-                                       className="flex px-3 py-2 md:px-4 md:py-3 border-2 border-black rounded-lg font-medium placeholder:font-normal"/>
-                                
-                                <input value="Add Offer" type="submit"
-                                       className="flex items-center justify-center flex-none px-3 py-2 md:px-4 md:py-3 border-2 rounded-lg font-medium border-black relative"/>
-
-                            </div>
-                        </form>
+      </div>
+      <div className=" min-h-screen w-full bg-white">
+        <div className="flex flex-row w-full">
+          <div className="flex flex-1 flex-col items-center justify-center px-10 relative">
+            <div className="flex flex-1 flex-col  justify-center space-y-5 max-w-md">
+              <div className="flex flex-col space-y-2 text-center">
+                <h2 className="text-3xl md:text-4xl font-bold">
+                  Add an offer{" "}
+                </h2>
+                <p className="text-md md:text-xl">test </p>
+              </div>
+              <form onSubmit={Addoffer}>
+                <div className="flex flex-col max-w-md space-y-5">
+                  <input
+                    onChange={(e) => setTitre(e.target.value)}
+                    type="text"
+                    placeholder="Titre"
+                    className="flex px-3 py-2 md:px-4 md:py-3 border-2 border-black rounded-lg font-medium placeholder:font-normal"
+                  />
+                  <select
+                    onChange={(e) => setSector(e.target.value)}
+                    className="flex px-3 py-2 md:px-4 md:py-3 border-2 border-black rounded-lg font-medium placeholder:font-normal"
+                  >
+                    <option value="IT">IT</option>
+                    <option value="RH">RH</option>
+                    <option value="MARKETING">MARKETING</option>
+                  </select>
+                  <select
+                    onChange={(e) => setType(e.target.value)}
+                    className="flex px-3 py-2 md:px-4 md:py-3 border-2 border-black rounded-lg font-medium placeholder:font-normal"
+                  >
+                    <option value="PFE">PFE</option>
+                    <option value="PFA">PFA</option>
+                    <option value="OBSERVATION">OBSERVATION</option>
+                  </select>
+                  <div
+                    className="flex w-max gap-4"
+                    onChange={(e) => setPaid(e.target.value)}
+                  >
+                    <p class="flex items-center">Paid</p>
+                    <div class="flex items-center">
+                      <input
+                        type="radio"
+                        value="1"
+                        name="default-radio"
+                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                      />
+                      <label class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                        Yes
+                      </label>
                     </div>
-               </div>
-             </div>
-         </div>
-    </div>
-    
-       
-     
-    )
-}
+                    <div class="flex items-center">
+                      <input
+                        checked
+                        type="radio"
+                        value="0"
+                        name="default-radio"
+                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
+                      <label
+                        for="default-radio-2"
+                        class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                        No
+                      </label>
+                    </div>
+                  </div>
+                  <textarea
+                    onChange={(e) => setDescription(e.target.value)}
+                    placeholder="Description"
+                    className="flex px-3 py-2 md:px-4 md:py-3 border-2 border-black rounded-lg font-medium placeholder:font-normal"
+                  />
+                  <input
+                    onChange={(e) => setNbrcand(e.target.value)}
+                    type="number"
+                    placeholder="number"
+                    className="flex px-3 py-2 md:px-4 md:py-3 border-2 border-black rounded-lg font-medium placeholder:font-normal"
+                  />
+                  <input
+                    onChange={(e) => setDatedebut(e.target.value)}
+                    type="date"
+                    min="2018-01-01"
+                    max="2023-12-31"
+                    className="flex px-3 py-2 md:px-4 md:py-3 border-2 border-black rounded-lg font-medium placeholder:font-normal"
+                  />
+                  <input
+                    onChange={(e) => setDatefin(e.target.value)}
+                    type="date"
+                    min="2018-01-01"
+                    max="2023-12-31"
+                    className="flex px-3 py-2 md:px-4 md:py-3 border-2 border-black rounded-lg font-medium placeholder:font-normal"
+                  />
 
-export default Addoffer
+                  <input
+                    value="Add Offer"
+                    type="submit"
+                    className="flex items-center justify-center flex-none px-3 py-2 md:px-4 md:py-3 border-2 rounded-lg font-medium border-black relative"
+                  />
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Addoffer;
